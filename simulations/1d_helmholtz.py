651ac7db62ded0ea77e491960f12de3f7c59523f
Max-Felix Müller <mxfxmmueller@gmail.com>
2024-12-26 17:18:06 +0100

    visualize field

--- simulations/1d_helmholtz.py
+++ simulations/1d_helmholtz.py
@@ -1,6 +1,7 @@
 import numpy as np
 import magpylib as magpy
 import matplotlib.pyplot as plt
+from math import sqrt
 
 # Set some parameters for the design
 WIRE_DIAMETER = 1 # [mm]
@@ -11,29 +12,33 @@ CURRENT = 1 # [A]
 # Create a finite sized Helmholtz coil-pair
 coil1 = magpy.Collection()
 windingcount = 0
-for n1 in range(WINDINGS_PER_COIL//2):
-    for n2 in range(WINDINGS_PER_COIL//2):
+for n1 in range(int(sqrt(WINDINGS_PER_COIL))):
+    for n2 in range(int(sqrt(WINDINGS_PER_COIL))):
         windingcount = windingcount + 1
         winding = magpy.current.Circle(
             current=CURRENT,
-            diameter=INNER_COIL_DIAMETER + n2 * WIRE_DIAMETER,
-            position=(0, 0, (n1+0.5)*WIRE_DIAMETER), # offset by 1/2 diameter
+            diameter=INNER_COIL_DIAMETER/1000 + n2 * WIRE_DIAMETER/1000,
+            position=(0, 0, (n1+0.5)*WIRE_DIAMETER/1000), # offset by 1/2 diameter
         )
         coil1.add(winding)
 print(f"Using {windingcount} windings per coil")
 
-coil1.position = (0,0,INNER_COIL_DIAMETER/4)
-coil2 = coil1.copy(position=(0,0,-INNER_COIL_DIAMETER/4))
+coil1.position = (0,0,INNER_COIL_DIAMETER/4000)
+coil2 = coil1.copy(position=(0,0,-INNER_COIL_DIAMETER/4000))
 helmholtz = magpy.Collection(coil1, coil2)
 
 helmholtz.show()
 
-quit()
+print(f"Field in center: {magpy.getB(helmholtz, [0, 0, 0])}")
 
-fig, ax = plt.subplots(1, 1, figsize=(6,5))
+
+
+fig, axes = plt.subplots(1, 2, figsize=(6,5))
+
+ax = axes[0]
 
 # Compute field and plot the coil pair field on yz-grid
-grid = np.mgrid[0:0:1j, -13:13:20j, -13:13:20j].T[:,:,0]
+grid = np.mgrid[0:0:1j, -0.5:0.5:20j, -0.5:0.5:20j].T[:,:,0]
 _, Y, Z = np.moveaxis(grid, 2, 0)
 
 B = magpy.getB(helmholtz, grid)
@@ -47,9 +52,9 @@ sp = ax.streamplot(Y, Z, By, Bz, density=2, color=Bamp,
 )
 
 # Plot coil outline
-from matplotlib.patches import Rectangle
-for loc in [(4,4), (4,-6), (-6,4), (-6,-6)]:
-    ax.add_patch(Rectangle(loc, 2, 2, color='k', zorder=10))
+#from matplotlib.patches import Rectangle
+#for loc in [(4,4), (4,-6), (-6,4), (-6,-6)]:
+#    ax.add_patch(Rectangle(loc, 2, 2, color='k', zorder=10))
 
 # Figure styling
 ax.set(
@@ -59,6 +64,40 @@ ax.set(
     aspect=1,
 )
 plt.colorbar(sp.lines, ax=ax, label='(T)')
+
+
+
+ax2 = axes[1]
+
+# Compute field and plot the coil pair field on yz-grid
+grid = np.mgrid[0:0:1j, -0.1:0.1:20j, -0.1:0.1:20j].T[:,:,0]
+_, Y, Z = np.moveaxis(grid, 2, 0)
+
+B = magpy.getB(helmholtz, grid)
+_, By, Bz = np.moveaxis(B, 2, 0)
+
+Bamp = np.linalg.norm(B, axis=2)
+Bamp /= np.amax(Bamp)
+
+sp = ax2.streamplot(Y, Z, By, Bz, density=2, color=Bamp,
+    linewidth=np.sqrt(Bamp)*3, cmap='coolwarm',
+)
+
+# Plot coil outline
+#from matplotlib.patches import Rectangle
+#for loc in [(4,4), (4,-6), (-6,4), (-6,-6)]:
+#    ax.add_patch(Rectangle(loc, 2, 2, color='k', zorder=10))
+
+# Figure styling
+ax2.set(
+    title='Magnetic field of Helmholtz',
+    xlabel='y-position (m)',
+    ylabel='z-position (m)',
+    aspect=1,
+)
+plt.colorbar(sp.lines, ax=ax2, label='(T)')
+
+
 
 plt.tight_layout()
 plt.show()
